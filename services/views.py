2c7ca1b665387cb4d4e35f488347977eeffd33a2
Ahmed Sayed local <ahmedsayed124680@gmail.com>
2022-12-03 19:39:15 +0200

    edit letter is not working still try 2

--- services/views.py
+++ services/views.py
@@ -11,6 +11,7 @@ from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from django.urls import reverse_lazy
 
 class priceBaseView(View):
@@ -29,6 +30,10 @@ class ServicePriceUpdateView(priceBaseView, UpdateView):
 class ServicePriceDeleteView(priceBaseView, DeleteView):
     """View to delete a film"""
 
+class EditLetter(UpdateView, pk):
+    model = Letter
+    fields = '__all__'
+    success_url = reverse_lazy('services:PrintLetter/255', kwargs={"letter_id": post.pk})
 
 class cancelLetter(APIView):
     def get(self, request, pk):
